88a5948257640ecfc631c13881f702154edd1003
tayleredelen <taylerledelen@gmail.com>
2023-05-04 20:01:41 -0500

    Finished chapter 14 studio

---
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -0,0 +1,40 @@
+package org.launchcode.spaday.controllers;
+
+
+import org.launchcode.spaday.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping("add")
+    public String displayAddUserForm(String username, String email, String password) {
+        return "user/add";
+    }
+
+    @PostMapping("")
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        // add form submission handling code here
+        if (verify == user.getPassword()) {
+            return "user/index";
+        } else {
+            model.addAttribute("error", "Passwords do not match");
+            model.addAttribute("username", user.getUsername());
+            model.addAttribute("email", user.getEmail());
+            return "serviceSelection";
+        }
+    }
+
+
+
+}
+
+
+
+
